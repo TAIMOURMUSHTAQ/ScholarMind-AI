6e87634383e7b39b3c22ea599f4d06d95f748ae0
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-14 16:48:15 -0300

    Extract data from pdf

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ doc = pymupdf.open("User_Guide NUS Summer Internship.pdf")
 #     print(page.get_text())
 
 #Meta Deta Extraction
-page = doc[0]
+# page = doc[0]
 
-blocks = page.get_text("dict")["blocks"]
-for block in blocks:
-    if block["type"] == 0:  # text block
-        for line in block["lines"]:
-            for span in line["spans"]:
-                print(f"{span['text']!r}  font={span['font']}  size={span['size']:.1f}")
-    
+# blocks = page.get_text("dict")["blocks"]
+# for block in blocks:
+#     if block["type"] == 0:  # text block
+#         for line in block["lines"]:
+#             for span in line["spans"]:
+#                 print(f"{span['text']!r}  font={span['font']}  size={span['size']:.1f}")
+
+#Renders Tables in PDF
+page = doc[0] 
+tables = page.find_tables()
+for table in tables:
+    print(table.to_markdown())
+
+    # or get as Pandas DataFrame
+    df = table.to_pandas()
