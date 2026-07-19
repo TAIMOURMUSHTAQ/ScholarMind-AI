01a8fa22e9d9a77c088a032aca53a7dcae67e16f
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-19 12:39:28 -0300

    Wrote the code to read the pdf

--- src/reader/pdf_reader.py
+++ src/reader/pdf_reader.py
@@ -3,6 +3,7 @@ PDF Reader
 Reads PDF files and converts them into Document objects
 """
 import fitz #PymuPdf
+import os
 from src.models.document import Document
 from src.models.page import Page
 
@@ -15,4 +16,32 @@ class PDFReader:
         """Read a PDF File
         Returns:Document
         """
-        raise NotImplementedError("Will be implemented in the next lesson")
+        doc = fitz.open(self.pdf_path)
+
+        pages = []
+        full_text = ""
+
+        try:
+            for page_number, page in enumerate(doc, start=1):
+                text = page.get_text()
+                page_obj = Page(
+                    page_number=page_number,
+                    text=text,
+                    word_count=len(text.split()),
+                    character_count=len(text)
+                )
+                pages.append(page_obj)
+                full_text += text + "\n"
+
+            document = Document(
+                file_name=os.path.basename(self.pdf_path),
+                file_path=self.pdf_path,
+                page_count=doc.page_count,
+                metadata=doc.metadata,
+                pages=pages,
+                full_text=full_text
+            )
+            return document
+        finally:
+            doc.close()
+        # raise NotImplementedError("Will be implemented in the next lesson")
