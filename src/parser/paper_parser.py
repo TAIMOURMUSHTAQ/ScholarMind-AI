3b9099f1b2195430ae71b050f3518a73520cf086
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-23 12:02:42 -0300

    Update the Paper Parser functions

--- src/parser/paper_parser.py
+++ src/parser/paper_parser.py
@@ -1,6 +1,16 @@
+import pymupdf
 from src.models.paper import Paper
-
+from src.extractors.title_extractor import TitleExtractor
 class PaperParser:
-    def parse(self,pdf_path):
-        paper=Paper()
+    def parse(self, pdf_path):
+        # Open PDF
+        doc = pymupdf.open(pdf_path)
+        # First page
+        first_page = doc[0]
+        # Create Paper object
+        paper = Paper()
+        # Extract title
+        paper.title = TitleExtractor.extract(first_page)
+        # Close document
+        doc.close()
         return paper
