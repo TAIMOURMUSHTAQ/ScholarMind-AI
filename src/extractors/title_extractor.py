bdab09eb2634082906f1e26251f962384845a415
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-23 11:59:15 -0300

    Update the title extractor function

--- src/extractors/title_extractor.py
+++ src/extractors/title_extractor.py
@@ -1,5 +1,21 @@
-class TitleExtractor:
+# class TitleExtractor:
+#     @staticmethod
+#     def extract(page):
+#         page.get_text("dict")
+        
+import pymupdf
+class TitleExtracotr:
     @staticmethod
     def extract(page):
-        page.get_text("dict")
+
+        data=page.get_text("dict")
+        candidates=[]
+        #TODO:
+        #Loop through blocks
+        #Skip non-text blocks
+        #Loop through lines
+        #Loop through spans
+        #Save text, font size and y coordinates
+
+        #Return text having the largest font
         
