8b30cb4614e8564a2af5c9487dd76d61630f2a6b
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-26 15:02:04 -0300

    Updated the layout block

--- src/models/layout_block.py
+++ src/models/layout_block.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-
 @dataclass
 class LayoutBlock:
-    text:str
-    font_size:float
-    x0:float
-    y0:float
-    x1:float
-    y1:float
-    block_number:int
+    text: str
+    font_size: float
+    x0: float
+    y0: float
+    x1: float
+    y1: float
+    block_number: int
+    page_number: int = 0
